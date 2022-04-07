54d92878f2f84e8617e6310505c951595cc74f37
yasenbr <74763207+yasenbr@users.noreply.github.com>
2022-04-07 14:52:42 +0300

    header build

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,10 @@
 import Head from "next/head";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>Amazon 2.0</title>
-      </Head>
-      <h1>Amazon app clone build</h1>
-      {/*Header */}
+      <Header />
     </div>
   );
 }
