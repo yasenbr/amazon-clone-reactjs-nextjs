7ce34c78d7f4249b485d5f0bb80264566dbac9df
yasenbr <74763207+yasenbr@users.noreply.github.com>
2022-04-07 17:10:44 +0300

    build products list

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,29 @@
 import Head from "next/head";
+import Banner from "../components/Banner";
 import Header from "../components/Header";
+import ProductFeed from "../components/ProductFeed";
 
-export default function Home() {
+export default function Home({ products }) {
   return (
-    <div>
+    <div className="bg-gray-100">
       <Header />
+      <main className="max-w-screen-2xl mx-auto">
+        {/**Banner */}
+        <Banner />
+        {/**Product feed */}
+        <ProductFeed products={products} />
+      </main>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const products = await fetch("https://fakestoreapi.com/products").then(
+    (res) => res.json()
+  );
+  return {
+    props: {
+      products,
+    },
+  };
 }
